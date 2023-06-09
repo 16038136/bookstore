827d0053ab769ba835d12bc1301c2a41af7c4e59
16038136 <108315980+16038136@users.noreply.github.com>
2023-06-09 15:21:07 +0530

    Add files via upload

---
+++ public/build/precache-manifest.156c4e680f5d1d792f2433e7ea830764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb75c38f8af22f399301de1661e5170",
+    "url": "/react-bookstore-cart/index.html"
+  },
+  {
+    "revision": "efc924825a0add6772cf",
+    "url": "/react-bookstore-cart/static/css/main.a2e33dd7.chunk.css"
+  },
+  {
+    "revision": "91de5f5904d33e4ffca9",
+    "url": "/react-bookstore-cart/static/js/2.4268ab66.chunk.js"
+  },
+  {
+    "revision": "7c8b7808c34cb59049374f5d75e06b5f",
+    "url": "/react-bookstore-cart/static/js/2.4268ab66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc924825a0add6772cf",
+    "url": "/react-bookstore-cart/static/js/main.e734040d.chunk.js"
+  },
+  {
+    "revision": "79c08841c863220d0bb5",
+    "url": "/react-bookstore-cart/static/js/runtime-main.e607833a.js"
+  },
+  {
+    "revision": "23e5e9bfcd13a654e9fe901925654a23",
+    "url": "/react-bookstore-cart/static/media/README.23e5e9bf.md"
+  }
+]);
